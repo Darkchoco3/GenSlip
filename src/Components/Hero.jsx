2e84b7e8fbd15ce07a079206ae0b0ba574f155b9
steph <ntuigoodness3@gmail.com>
2024-06-26 11:38:02 +0100

    second commit

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from "react";
 import Pause from "../assets/pattern-divider-desktop.svg";
 import Greenbox from "../assets/icon-dice.svg";
 import "../Styles/Hero.css";
+
 const Hero = () => {
-  const [advice, setAdvice] = useState(" ");
+  const [advice, setAdvice] = useState("");
   const [slipId, setSlipId] = useState(null);
 
   const getSlip = async () => {
     try {
-      const response = await fetch('https://api.adviceslip.com/advice');
+      const response = await fetch("https://api.adviceslip.com/advice");
       const data = await response.json();
       setAdvice(data.slip.advice);
       setSlipId(data.slip.id);
@@ -16,39 +17,22 @@ const Hero = () => {
       console.error(error.message);
     }
   };
+
   useEffect(() => {
-      getSlip();
-    }, []);
+    getSlip();
+  }, []);
+
   return (
-    <>
-      {/* <main> */}
-      <div>
-      <div className="first-box">
+    <div className="first-box">
       <div className="quote-container">
-        <h2  className="advise">ADVICE #{slipId} </h2>
-        <p>
-    {advice}
-        </p>
+        <h2 className="advise">ADVICE #{slipId}</h2>
+        <p>{advice}</p>
         <img src={Pause} className="text-container" alt="" />
-
-        <button className="button-container" onClick={getSlip} >
-        <img src={Greenbox} alt="" />
+        <button className="button-container" onClick={getSlip}>
+          <img src={Greenbox} alt="" />
         </button>
-
-
       </div>
-    {/* second div */}
-
-      {/* <div className='green-box'> */}
-        {/* <button className="button-container" onClick={getSlip} >
-        <img src={Greenbox} alt="" />
-        </button> */}
-        {/* <img src={Greenbox} alt="" /> */}
-    
-</div>
-</div>
-    {/* </div> */}
-    </>
+    </div>
   );
 };
 
